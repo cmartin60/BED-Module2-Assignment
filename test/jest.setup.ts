819c8e238449585bbae2f39a125c374590e50d4f
Christian Martin <cmartin60@academic.rrc.ca>
2025-11-01 03:44:53 -0500

    Edit jest setup to remove tests errors.

--- test/jest.setup.ts
+++ test/jest.setup.ts
@@ -1,5 +1,19 @@
-module.exports = {
-    preset: "ts-jest",
-    testEnvironment: "node",
-    setupFilesAfterEnv: ["<rootDir>/tests/jest.setup.ts"],
-};
+import dotenv from "dotenv";
+
+dotenv.config();
+
+if (!process.env.FIREBASE_PROJECT_ID) {
+    process.env.FIREBASE_PROJECT_ID = "test-project";
+}
+
+if (!process.env.FIREBASE_CLIENT_EMAIL) {
+    process.env.FIREBASE_CLIENT_EMAIL = "test@local.test";
+}
+
+if (!process.env.FIREBASE_PRIVATE_KEY) {
+    process.env.FIREBASE_PRIVATE_KEY =
+        "-----BEGIN PRIVATE KEY-----\nTEST_PRIVATE_KEY_FOR_UNIT_TESTS\n-----END PRIVATE KEY-----\n";
+}
+
+
+export {};
